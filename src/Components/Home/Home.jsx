d34286879ac4818613f3d0243cc177527b52ac15
Harsh Vardhan Mishra <138967484+HARSH-VARDHAN-MISHRA@users.noreply.github.com>
2024-06-05 16:46:42 +0530

    change in product component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,6 +6,8 @@ import Dashboard from '../../Pages/Dashboard/Dashboard'
 import AllCategory from '../../Pages/Category/AllCategory'
 import AddCategory from '../../Pages/Category/AddCategory'
 import EditCategory from '../../Pages/Category/EditCategory'
+import AllProduct from '../../Pages/Products/AllProduct'
+import AddProduct from '../../Pages/Products/AddProduct'
 
 const Home = () => {
   return (
@@ -16,9 +18,15 @@ const Home = () => {
         <Routes>
           <Route path={"/dashboard"} element={<Dashboard/>}/>
 
+          {/* Category --  */}
           <Route path={"/all-category"} element={<AllCategory/>}/>
           <Route path={"/add-category"} element={<AddCategory/>}/>
           <Route path={"/edit-category/:id"} element={<EditCategory/>}/>
+
+          {/* Product --  */}
+          <Route path={"/all-product"} element={<AllProduct/>}/>
+          <Route path={"/add-product"} element={<AddProduct/>}/>
+          <Route path={"/edit-product/:id"} element={<EditCategory/>}/>
 
         </Routes>
       </div>
